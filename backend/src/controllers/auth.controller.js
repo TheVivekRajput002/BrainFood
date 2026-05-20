0818791d929aebc30c230f1fac8932c141988f9e
Vivek Rajput <work.tvivekrajput@gmail.com>
2026-05-20 16:36:58 +0530

    foxed cross site error

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -35,7 +35,12 @@ async function registerUser(req, res) {
             id: user._id
         }, process.env.JWT_SECRET)
 
-        res.cookie("token", token)
+        res.cookie("token", token, {
+            httpOnly: true,
+            secure: true,
+            sameSite: "none",
+            maxAge: 7 * 24 * 60 * 60 * 1000
+        });
 
         res.status(201).json({
             message: "user registered successfully",
@@ -78,7 +83,12 @@ async function loginUser(req, res) {
             id: user._id
         }, process.env.JWT_SECRET)
 
-        res.cookie("token", token)
+        res.cookie("token", token, {
+  httpOnly: true,
+  secure: true,
+  sameSite: "none",
+  maxAge: 7 * 24 * 60 * 60 * 1000
+});
 
         res.status(200).json({
             message: "logged in successfully",
@@ -97,7 +107,7 @@ async function loginUser(req, res) {
 }
 
 function logoutUser(req, res) {
-    res.clearCookie("token");
+    res.clearCookie("token", { httpOnly: true, secure: true, sameSite: "none" });
     res.status(200).json({
         message: "logged out successfully"
     })
@@ -128,7 +138,12 @@ async function registerFoodPartner(req, res) {
         id: foodPartner._id
     }, process.env.JWT_SECRET)
 
-    res.cookie("token", token)
+   res.cookie("token", token, {
+  httpOnly: true,
+  secure: true,
+  sameSite: "none",
+  maxAge: 7 * 24 * 60 * 60 * 1000
+});
 
     res.status(201).json({
         message: "foodpartner registered successfully",
@@ -166,7 +181,12 @@ async function loginFoodPartner(req, res) {
         id: foodPartner._id
     }, process.env.JWT_SECRET)
 
-    res.cookie("token", token)
+  res.cookie("token", token, {
+  httpOnly: true,
+  secure: true,
+  sameSite: "none",
+  maxAge: 7 * 24 * 60 * 60 * 1000
+});
 
     res.status(201).json({
         message: "logged in succesfuly",
@@ -180,7 +200,7 @@ async function loginFoodPartner(req, res) {
 }
 
 function logoutFoodPartner(req, res) {
-    res.clearCookie("token");
+    res.clearCookie("token", { httpOnly: true, secure: true, sameSite: "none" });
     res.status(200).json({
         message: "food partner logged out successfully"
     })
